16c1751ce2b8f5725ceff6e475342d0ae93bc1f2
tejasweeA <tejaswee.1@iitj.ac.in>
2021-10-07 22:50:37 +0530

    adding test branch

--- classification_digits/utils.py
+++ classification_digits/utils.py
@@ -2,12 +2,19 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets, svm, metrics
 
 
-def split_dataset(data,target,test_size):
+def split_dataset(data,target,test_size,val_size):
 
-    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size= test_size, random_state=1)
-    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=1)
+    X_train, X_test_val, y_train, y_test_val = train_test_split(data, target, test_size= test_size+val_size, shuffle=False)
+    X_test, X_val, y_test, y_val = train_test_split(X_test_val, y_test_val, test_size=val_size / (test_size + val_size), shuffle=False)
 
     return X_train, X_test, X_val, y_train,y_test,y_val
+
+
+def model_selection(test_size,corr_gamma):
+    best_model_dir = "../model_select/valid_{}_gamma_{}".format((test_size),corr_gamma)
+    return best_model_dir
+
+
 
 def train_model(clf,X,y):
 
@@ -18,6 +25,6 @@ def train_model(clf,X,y):
     model_values["acc"] = acc
     model_values["f1"] = f1_score
 
-    return model_values	
+    return model_values
 
 
---
+++ tests/test_examples.py
@@ -0,0 +1,35 @@
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+import os
+import utils
+
+digits = datasets.load_digits()
+test_size = 0.15
+val_size=0.15
+gamma_val = [0.00001,0.0001,0.001,0.01]
+
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+
+for gm_val in gamma_val:
+    clf = svm.SVC(gamma=gm_val)
+
+
+    X_train, X_test, X_val, y_train,y_test,y_val = utils.split_dataset(data,digits.target,test_size,val_size)
+    clf.fit(X_train, y_train)
+
+    train_metrics = utils.train_model(clf,X_train,y_train)
+
+
+    # TODO-1
+
+    def test_file_path():
+        best_model_dir = utils.model_selection(test_size,gm_val)
+        print(best_model_dir)
+        assert os.path.exists(best_model_dir)
+
+    #TODO-2
+
+    def test_small_data_overfit_checking():
+        assert train_metrics['acc']  > 0.11
+        assert train_metrics['f1'] > 0.11
